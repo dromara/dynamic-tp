1a1c4cc5180f69beff44dc3110a73e9ba79ef9ec
yanhom <linyanhong@ihuman.com>
2022-01-10 20:17:59 +0800

    add logging listener

---
+++ src/main/java/io/lyh/dtp/logging/DtpLoggingListener.java
@@ -0,0 +1,37 @@
+package io.lyh.dtp.logging;
+
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.event.GenericApplicationListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.ResolvableType;
+
+/**
+ * Reload dtp log configuration file, after
+ * {@link org.springframework.boot.context.logging.LoggingApplicationListener}.
+ *
+ * @author: yanhom
+ * @since 1.0.0
+ **/
+public class DtpLoggingListener implements GenericApplicationListener {
+
+    @Override
+    public boolean supportsEventType(ResolvableType resolvableType) {
+        Class<?> type = resolvableType.getRawClass();
+        if (type != null) {
+            return ApplicationEnvironmentPreparedEvent.class.isAssignableFrom(type);
+        }
+        return false;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent applicationEvent) {
+        DtpLogging.getInstance().loadConfiguration();
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE + 31;
+    }
+
+}

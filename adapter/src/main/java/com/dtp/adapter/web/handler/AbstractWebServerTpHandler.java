2244b8520ebb1bac5261778ab681f7597bdea480
yanhom <yanhom1314@foxmail.com>
2022-02-26 21:43:49 +0800

    fix tomcat webserver start error sometimes

--- adapter/src/main/java/com/dtp/adapter/web/handler/AbstractWebServerTpHandler.java
+++ adapter/src/main/java/com/dtp/adapter/web/handler/AbstractWebServerTpHandler.java
@@ -2,10 +2,12 @@ package com.dtp.adapter.web.handler;
 
 import com.dtp.common.ApplicationContextHolder;
 import com.dtp.common.config.DtpProperties;
-import org.springframework.beans.factory.InitializingBean;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.context.WebServerApplicationContext;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
 
 import java.util.concurrent.Executor;
 
@@ -15,9 +17,21 @@ import java.util.concurrent.Executor;
  * @author yanhom
  * @since 1.0.0
  */
-public abstract class AbstractWebServerTpHandler implements WebServerTpHandler, InitializingBean {
+@Slf4j
+public abstract class AbstractWebServerTpHandler
+        implements WebServerTpHandler, ApplicationListener<ServletWebServerInitializedEvent> {
 
     protected volatile Executor webServerExecutor;
+
+    @Override
+    public void onApplicationEvent(ServletWebServerInitializedEvent event) {
+        try {
+            DtpProperties dtpProperties = ApplicationContextHolder.getBean(DtpProperties.class);
+            updateWebServerTp(dtpProperties);
+        } catch (Exception e) {
+            log.error("Init web server thread pool failed.", e);
+        }
+    }
 
     @Override
     public Executor getWebServerTp() {
@@ -40,10 +54,4 @@ public abstract class AbstractWebServerTpHandler implements WebServerTpHandler, 
      * @return the Executor instance
      */
     protected abstract Executor doGetTp(WebServer webServer);
-
-    @Override
-    public void afterPropertiesSet() {
-        DtpProperties dtpProperties = ApplicationContextHolder.getBean(DtpProperties.class);
-        updateWebServerTp(dtpProperties);
-    }
 }

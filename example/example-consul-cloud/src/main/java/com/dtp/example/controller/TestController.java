b32544779adb109e63343b9dcb266c2819640783
Redick01 <11090829@qq.com>
2022-03-20 21:16:46 +0800

    support consul config center and add consul application example

---
+++ example/example-consul-cloud/src/main/java/com/dtp/example/controller/TestController.java
@@ -0,0 +1,47 @@
+package com.dtp.example.controller;
+
+import com.dtp.core.DtpRegistry;
+import com.dtp.core.thread.DtpExecutor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author Redick01
+ */
+@RestController
+@Slf4j
+@SuppressWarnings("all")
+public class TestController {
+
+    @Resource
+    private ThreadPoolExecutor dtpExecutor1;
+
+    @GetMapping("/dtp-zookeeper-example/test")
+    public String test() {
+        new Thread(() -> {
+            try {
+                task();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        return "success";
+    }
+
+    public void task() throws InterruptedException {
+        DtpExecutor dtpExecutor2 = DtpRegistry.getDtpExecutor("dtpExecutor2");
+        for (int i = 0; i < 100; i++) {
+            Thread.sleep(100);
+            dtpExecutor1.execute(() -> {
+                log.info("i am dynamic-tp-test-1 task");
+            });
+            dtpExecutor2.execute(() -> {
+                log.info("i am dynamic-tp-test-2 task");
+            });
+        }
+    }
+}

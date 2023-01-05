48f47186a1bab394c5b356e758d2d9ae6279a57b
yanhom <yanhom1314@foxmail.com>
2023-01-05 23:34:58 +0800

    format code

--- adapter/adapter-rocketmq/src/main/java/com/dtp/adapter/rocketmq/StreamRocketMqDtpAdapter.java
+++ adapter/adapter-rocketmq/src/main/java/com/dtp/adapter/rocketmq/StreamRocketMqDtpAdapter.java
@@ -13,10 +13,11 @@ import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.impl.consumer.ConsumeMessageConcurrentlyService;
 import org.apache.rocketmq.client.impl.consumer.ConsumeMessageOrderlyService;
 import org.apache.rocketmq.client.impl.consumer.DefaultMQPushConsumerImpl;
+import org.springframework.cloud.stream.binder.Binding;
 import org.springframework.cloud.stream.binding.BindingService;
 
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -27,6 +28,7 @@ import java.util.concurrent.ThreadPoolExecutor;
  * @since 1.1.0
  */
 @Slf4j
+@SuppressWarnings("all")
 public class StreamRocketMqDtpAdapter extends AbstractDtpAdapter {
 
     private static final String NAME = "rocketMqTp";
@@ -44,32 +46,42 @@ public class StreamRocketMqDtpAdapter extends AbstractDtpAdapter {
 
         val beans = ApplicationContextHolder.getBeansOfType(BindingService.class);
         if (CollUtil.isEmpty(beans)) {
-            log.warn("Cannot find beans of type DefaultRocketMQListenerContainer.");
+            log.warn("Cannot find beans of type BindingService.");
             return;
         }
 
         beans.forEach((bindingServiceName, bindingService) -> {
-            val consumerBindings = (HashMap<String, List>) ReflectionUtil.getFieldValue(BindingService.class, "consumerBindings", bindingService);
+            val consumerBindings = (Map<String, List<Binding<?>>>) ReflectionUtil.getFieldValue(BindingService.class,
+                    "consumerBindings", bindingService);
+            if (CollUtil.isEmpty(consumerBindings)) {
+                return;
+            }
             consumerBindings.forEach((bindingName, messageChannelBinders) -> {
                 val messageChannelBinder = messageChannelBinders.get(0);
                 Class<?> messageChannelBinderClass = messageChannelBinder.getClass();
-                val lifecycle = (RocketMQInboundChannelAdapter) ReflectionUtil.getFieldValue(messageChannelBinderClass, "lifecycle", messageChannelBinder);
-                val pushConsumer = (DefaultMQPushConsumer) ReflectionUtil.getFieldValue(RocketMQInboundChannelAdapter.class, "pushConsumer", lifecycle);
-                val defaultMQPushConsumerImpl = (DefaultMQPushConsumerImpl) ReflectionUtil.getFieldValue(DefaultMQPushConsumer.class, "defaultMQPushConsumerImpl", pushConsumer);
+                val lifecycle = (RocketMQInboundChannelAdapter) ReflectionUtil.getFieldValue(messageChannelBinderClass,
+                        "lifecycle", messageChannelBinder);
+                val pushConsumer = (DefaultMQPushConsumer) ReflectionUtil.getFieldValue(RocketMQInboundChannelAdapter.class,
+                        "pushConsumer", lifecycle);
+                val defaultMQPushConsumerImpl = (DefaultMQPushConsumerImpl) ReflectionUtil.getFieldValue(DefaultMQPushConsumer.class,
+                        "defaultMQPushConsumerImpl", pushConsumer);
                 if (defaultMQPushConsumerImpl == null || defaultMQPushConsumerImpl.getConsumeMessageService() == null) {
                     return;
                 }
                 val consumeMessageService = defaultMQPushConsumerImpl.getConsumeMessageService();
                 ThreadPoolExecutor executor = null;
                 if (consumeMessageService instanceof ConsumeMessageConcurrentlyService) {
-                    executor = (ThreadPoolExecutor) ReflectionUtil.getFieldValue(ConsumeMessageConcurrentlyService.class, CONSUME_EXECUTOR_FIELD_NAME, consumeMessageService);
+                    executor = (ThreadPoolExecutor) ReflectionUtil.getFieldValue(ConsumeMessageConcurrentlyService.class,
+                            CONSUME_EXECUTOR_FIELD_NAME, consumeMessageService);
                 } else if (consumeMessageService instanceof ConsumeMessageOrderlyService) {
-                    executor = (ThreadPoolExecutor) ReflectionUtil.getFieldValue(ConsumeMessageOrderlyService.class, CONSUME_EXECUTOR_FIELD_NAME, consumeMessageService);
+                    executor = (ThreadPoolExecutor) ReflectionUtil.getFieldValue(ConsumeMessageOrderlyService.class,
+                            CONSUME_EXECUTOR_FIELD_NAME, consumeMessageService);
                 }
                 if (Objects.nonNull(executor)) {
-                    val destination = (String) ReflectionUtil.getFieldValue(messageChannelBinderClass, "name", messageChannelBinder);
-                    val group = (String) ReflectionUtil.getFieldValue(messageChannelBinderClass, "group", messageChannelBinder);
-
+                    val destination = (String) ReflectionUtil.getFieldValue(messageChannelBinderClass,
+                            "name", messageChannelBinder);
+                    val group = (String) ReflectionUtil.getFieldValue(messageChannelBinderClass,
+                            "group", messageChannelBinder);
                     String key = group + "#" + destination;
                     val executorWrapper = new ExecutorWrapper(key, executor);
                     initNotifyItems(key, executorWrapper);

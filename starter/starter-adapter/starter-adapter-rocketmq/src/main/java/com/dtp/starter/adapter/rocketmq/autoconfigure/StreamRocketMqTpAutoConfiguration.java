f2f2779ad6be2f5dd969d4470926cf0329819bf5
占金兵 <592466695@qq.com>
2023-01-05 22:48:51 +0800

    为spring cloud stream rocketMq添加starter 自动配置类

---
+++ starter/starter-adapter/starter-adapter-rocketmq/src/main/java/com/dtp/starter/adapter/rocketmq/autoconfigure/StreamRocketMqTpAutoConfiguration.java
@@ -0,0 +1,28 @@
+package com.dtp.starter.adapter.rocketmq.autoconfigure;
+
+import com.dtp.adapter.rocketmq.StreamRocketMqDtpAdapter;
+import com.dtp.starter.common.autoconfigure.BaseBeanAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * StreamRocketMqTpAutoConfiguration related
+ *
+ * @author zhanjb
+ * @since 1.1.0
+ */
+@Configuration
+@ConditionalOnProperty(prefix = "spring.cloud.stream.rocketmq.binder", value = {"name-server"})
+@AutoConfigureAfter({BaseBeanAutoConfiguration.class})
+@SuppressWarnings("all")
+public class StreamRocketMqTpAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public StreamRocketMqDtpAdapter rocketMqDtpHandler() {
+        return new StreamRocketMqDtpAdapter();
+    }
+}

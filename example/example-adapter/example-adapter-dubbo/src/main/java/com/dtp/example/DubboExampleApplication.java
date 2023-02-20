74bae2eaf68a4cc0f69f1cae4e0a5d6b744af80b
fabian <fabian4@163.com>
2023-02-20 22:34:28 +0800

    refactor example-adapter

--- example/example-adapter/example-adapter-dubbo/src/main/java/com/dtp/example/DubboExampleApplication.java
+++ example/example-adapter/example-adapter-dubbo/src/main/java/com/dtp/example/DubboExampleApplication.java
@@ -1,12 +1,14 @@
 package com.dtp.example;
 
 import com.dtp.core.spring.EnableDynamicTp;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author fabian
  */
+@EnableDubbo
 @EnableDynamicTp
 @SpringBootApplication
 public class DubboExampleApplication {

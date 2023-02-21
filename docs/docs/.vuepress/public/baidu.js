f991523f37d2d4ed229fc2dec10e8ab53649b2cd
yanhom <yanhom1314@foxmail.com>
2023-02-21 14:25:28 +0800

    update baidu.js

--- docs/docs/.vuepress/public/baidu.js
+++ docs/docs/.vuepress/public/baidu.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
-(function () {
+(function() {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?3f42577ac1e8b55e43134363d33f2e80";
+    hm.src = "https://hm.baidu.com/hm.js?271bc4a4c9eba8c190a787fa6145291a";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();

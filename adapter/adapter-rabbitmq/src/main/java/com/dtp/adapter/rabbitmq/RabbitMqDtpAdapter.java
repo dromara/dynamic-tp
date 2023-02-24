f47bc1d802dafaa0ea917416928c002f83cc5d08
fabian4 <fabian4@163.com>
2023-02-24 23:30:50 +0800

    add support for rabbitmq

--- adapter/adapter-rabbitmq/src/main/java/com/dtp/adapter/rabbitmq/RabbitMqDtpAdapter.java
+++ adapter/adapter-rabbitmq/src/main/java/com/dtp/adapter/rabbitmq/RabbitMqDtpAdapter.java
@@ -3,10 +3,19 @@ package com.dtp.adapter.rabbitmq;
 import com.dtp.adapter.common.AbstractDtpAdapter;
 import com.dtp.common.ApplicationContextHolder;
 import com.dtp.common.properties.DtpProperties;
+import com.dtp.common.util.ReflectionUtil;
+import com.dtp.core.support.ExecutorWrapper;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.commons.collections.MapUtils;
-import org.springframework.amqp.rabbit.listener.DirectReplyToMessageListenerContainer;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.AbstractConnectionFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+
+import java.util.Objects;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * RabbitMqDtpAdapter related
@@ -19,7 +28,7 @@ import org.springframework.amqp.rabbit.listener.DirectReplyToMessageListenerCont
 public class RabbitMqDtpAdapter extends AbstractDtpAdapter {
     private static final String NAME = "rabbitMqTp";
 
-    private static final String CONSUME_EXECUTOR_FIELD_NAME = "consumeExecutor";
+    private static final String CONSUME_EXECUTOR_FIELD_NAME = "executorService";
 
     @Override
     public void refresh(DtpProperties dtpProperties) {
@@ -30,43 +39,24 @@ public class RabbitMqDtpAdapter extends AbstractDtpAdapter {
     protected void initialize() {
         super.initialize();
 
-        adaptCustomerExecutors();
-//        adaptProducerExecutors();
-
-        log.info("DynamicTp adapter, rabbitmq consumer and producer executors init end, executors: {}", executors);
-    }
-
-    public void adaptCustomerExecutors() {
-
-        val beans = ApplicationContextHolder.getBeansOfType(DirectReplyToMessageListenerContainer.class);
+        val beans = ApplicationContextHolder.getBeansOfType(AbstractConnectionFactory.class);
         if (MapUtils.isEmpty(beans)) {
-            log.warn("Cannot find beans of type DefaultRocketMQListenerContainer.");
+            log.warn("Cannot find beans of type AbstractConnectionFactory.");
             return;
         }
         beans.forEach((k, v) -> {
-//            DefaultRocketMQListenerContainer container = (DefaultRocketMQListenerContainer) v;
-//            DefaultMQPushConsumer consumer = container.getConsumer();
-//            val pushConsumer = (DefaultMQPushConsumerImpl) ReflectionUtil.getFieldValue(DefaultMQPushConsumer.class,
-//                    "defaultMQPushConsumerImpl", consumer);
-//            if (Objects.isNull(pushConsumer)) {
-//                return;
-//            }
-//
-//            String cusKey = container.getConsumerGroup() + "#" + container.getTopic();
-//            ThreadPoolExecutor executor = null;
-//            val consumeMessageService = pushConsumer.getConsumeMessageService();
-//            if (consumeMessageService instanceof ConsumeMessageConcurrentlyService) {
-//                executor = (ThreadPoolExecutor) ReflectionUtil.getFieldValue(ConsumeMessageConcurrentlyService.class,
-//                        CONSUME_EXECUTOR_FIELD_NAME, consumeMessageService);
-//            } else if (consumeMessageService instanceof ConsumeMessageOrderlyService) {
-//                executor = (ThreadPoolExecutor) ReflectionUtil.getFieldValue(ConsumeMessageOrderlyService.class,
-//                        CONSUME_EXECUTOR_FIELD_NAME, consumeMessageService);
-//            }
-//            if (Objects.nonNull(executor)) {
-//                val executorWrapper = new ExecutorWrapper(cusKey, executor);
-//                initNotifyItems(cusKey, executorWrapper);
-//                executors.put(cusKey, executorWrapper);
-//            }
+            AbstractConnectionFactory abstractConnectionFactory = (AbstractConnectionFactory) v;
+            ThreadPoolExecutor executor = null;
+            executor = (ThreadPoolExecutor) ReflectionUtil.getFieldValue(AbstractConnectionFactory.class,
+                    CONSUME_EXECUTOR_FIELD_NAME, abstractConnectionFactory);
+
+            if (Objects.nonNull(executor)) {
+                val executorWrapper = new ExecutorWrapper(NAME, executor);
+                initNotifyItems(NAME, executorWrapper);
+                executors.put(NAME, executorWrapper);
+            }
         });
+
+        log.info("DynamicTp adapter, rabbitmq executors init end, executors: {}", executors);
     }
 }
--- example/example-adapter/example-adapter-rabbitmq/src/main/java/com/dtp/example/mq/RabbitMqConfig.java
+++ example/example-adapter/example-adapter-rabbitmq/src/main/java/com/dtp/example/mq/RabbitMqConfig.java
@@ -1,8 +1,16 @@
 package com.dtp.example.mq;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.AbstractRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.config.DirectRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.AbstractConnectionFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 /**
  * @author fabian4
@@ -13,5 +21,31 @@ public class RabbitMqConfig {
     @Bean
     public Queue Queue() {
         return new Queue("testQueue");
+    }
+
+    @Bean
+    public AbstractRabbitListenerContainerFactory<?> defaultRabbitListenerContainerFactory(AbstractConnectionFactory abstractConnectionFactory) {
+
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setMaxPoolSize(5);
+        executor.setCorePoolSize(5);
+        executor.setQueueCapacity(1000);
+        executor.setThreadNamePrefix("RabbitMqTaskExecutor-");
+
+        executor.initialize();
+
+        DirectRabbitListenerContainerFactory factory = new DirectRabbitListenerContainerFactory();
+        factory.setConnectionFactory(abstractConnectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        factory.setConsumersPerQueue(10);
+        abstractConnectionFactory.setExecutor(executor);
+        return factory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(CachingConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
     }
 }

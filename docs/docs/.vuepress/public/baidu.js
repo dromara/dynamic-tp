c30ff321d03815b517300e849310512e29b78572
yanhom <yanhom1314@foxmail.com>
2023-03-04 22:41:19 +0800

    remove docs

--- docs/docs/.vuepress/public/baidu.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?271bc4a4c9eba8c190a787fa6145291a";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();

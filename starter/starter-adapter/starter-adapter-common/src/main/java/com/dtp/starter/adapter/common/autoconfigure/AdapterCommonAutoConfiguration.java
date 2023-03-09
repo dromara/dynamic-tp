d5f5b2080b7d3edb93b742fa4d977fcc0e48f6f9
kamtohung <kamtohung@gmail.com>
2023-03-09 18:46:03 +0800

    check style

--- starter/starter-adapter/starter-adapter-common/src/main/java/com/dtp/starter/adapter/common/autoconfigure/AdapterCommonAutoConfiguration.java
+++ starter/starter-adapter/starter-adapter-common/src/main/java/com/dtp/starter/adapter/common/autoconfigure/AdapterCommonAutoConfiguration.java
@@ -2,12 +2,10 @@ package com.dtp.starter.adapter.common.autoconfigure;
 
 import com.dtp.adapter.common.DtpAdapterListener;
 import com.dtp.common.properties.DtpProperties;
-import com.dtp.common.constant.DynamicTpConst;
 import com.dtp.starter.common.autoconfigure.BaseBeanAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

96b5857bae863d7ee7ce450ea7b1756afba01169
ruoan <ruoan777@gmail.com>
2023-03-27 23:50:12 +0800

    当组件不启用时可以通过手动配置关闭

--- common/src/main/java/com/dtp/common/constant/DynamicTpConst.java
+++ common/src/main/java/com/dtp/common/constant/DynamicTpConst.java
@@ -20,6 +20,8 @@ public final class DynamicTpConst {
 
     public static final String MAIN_PROPERTIES_PREFIX = "spring.dynamic.tp";
 
+    public static final String DTP_ENABLED_PROP = MAIN_PROPERTIES_PREFIX + ".enabled";
+
     public static final String BANNER_ENABLED_PROP = MAIN_PROPERTIES_PREFIX + ".enabledBanner";
 
     public static final int AVAILABLE_PROCESSORS = Runtime.getRuntime().availableProcessors();
--- core/src/main/java/com/dtp/core/spring/DtpConfigurationSelector.java
+++ core/src/main/java/com/dtp/core/spring/DtpConfigurationSelector.java
@@ -1,8 +1,13 @@
 package com.dtp.core.spring;
 
+import org.apache.commons.lang3.BooleanUtils;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.Ordered;
+import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotationMetadata;
+
+import static com.dtp.common.constant.DynamicTpConst.DTP_ENABLED_PROP;
 
 /**
  * DtpConfigurationSelector related
@@ -10,10 +15,20 @@ import org.springframework.core.type.AnnotationMetadata;
  * @author KamTo Hung
  * @since 1.1.1
  */
-public class DtpConfigurationSelector implements DeferredImportSelector, Ordered {
+public class DtpConfigurationSelector implements DeferredImportSelector, Ordered, EnvironmentAware {
+
+    private Environment environment;
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
 
     @Override
     public String[] selectImports(AnnotationMetadata metadata) {
+        if (!BooleanUtils.toBoolean(environment.getProperty(DTP_ENABLED_PROP, BooleanUtils.TRUE))) {
+            return new String[]{};
+        }
         return new String[]{DtpBaseBeanConfiguration.class.getName(),
                 DtpBeanDefinitionRegistrar.class.getName()};
     }

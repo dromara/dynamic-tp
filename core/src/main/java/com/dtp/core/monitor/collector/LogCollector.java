469725063372bf501a810c5b0124d762b30dc665
KamTo Hung <kamtohung@gmail.com>
2023-04-12 14:55:10 +0800

    #194

--- core/src/main/java/com/dtp/core/monitor/collector/LogCollector.java
+++ core/src/main/java/com/dtp/core/monitor/collector/LogCollector.java
@@ -1,8 +1,8 @@
 package com.dtp.core.monitor.collector;
 
-import cn.hutool.json.JSONUtil;
-import com.dtp.common.entity.ThreadPoolStats;
 import com.dtp.common.em.CollectorTypeEnum;
+import com.dtp.common.entity.ThreadPoolStats;
+import com.dtp.common.util.JsonUtil;
 import com.dtp.logging.LogHelper;
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,7 +17,7 @@ public class LogCollector extends AbstractCollector {
 
     @Override
     public void collect(ThreadPoolStats threadPoolStats) {
-        String metrics = JSONUtil.toJsonStr(threadPoolStats);
+        String metrics = JsonUtil.toJson(threadPoolStats);
         if (LogHelper.getMonitorLogger() == null) {
             log.error("Cannot find monitor logger...");
             return;

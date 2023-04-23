36e2c902dda7fca0972652bdc895665d138f4f17
yanhom <yanhom1314@foxmail.com>
2023-04-23 16:55:45 +0800

    update test case

--- test/test-core/src/test/java/com/dtp/test/core/notify/capture/CapturedBlockingQueueTest.java
+++ test/test-core/src/test/java/com/dtp/test/core/notify/capture/CapturedBlockingQueueTest.java
@@ -2,11 +2,17 @@ package com.dtp.test.core.notify.capture;
 
 import com.dtp.common.queue.VariableLinkedBlockingQueue;
 import com.dtp.core.notifier.capture.CapturedBlockingQueue;
+import com.dtp.core.support.ThreadPoolBuilder;
+import com.dtp.core.thread.DtpExecutor;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import static com.dtp.common.em.QueueTypeEnum.VARIABLE_LINKED_BLOCKING_QUEUE;
 
 /**
  * CapturedBlockingQueueTest related
@@ -16,10 +22,28 @@ import java.util.concurrent.BlockingQueue;
  */
 public class CapturedBlockingQueueTest {
 
+    private static DtpExecutor dtpExecutor;
+
+    @BeforeAll
+    public static void setUp() {
+        dtpExecutor = ThreadPoolBuilder.newBuilder()
+                .threadPoolName("dtpExecutor1")
+                .corePoolSize(10)
+                .maximumPoolSize(15)
+                .keepAliveTime(15000)
+                .timeUnit(TimeUnit.MILLISECONDS)
+                .workQueue(VARIABLE_LINKED_BLOCKING_QUEUE.getName(), 100, false, null)
+                .waitForTasksToCompleteOnShutdown(true)
+                .awaitTerminationSeconds(5)
+                .runTimeout(200)
+                .queueTimeout(200)
+                .buildDynamic();
+    }
+
     @Test
     public void testBlockingQueueDefaultCapacity() {
-        BlockingQueue<Runnable> queue = new VariableLinkedBlockingQueue<>();
-        CapturedBlockingQueue capturedBlockingQueue = new CapturedBlockingQueue(queue);
+        ;
+        CapturedBlockingQueue capturedBlockingQueue = new CapturedBlockingQueue(dtpExecutor);
         Assertions.assertEquals(0, capturedBlockingQueue.size());
         Assertions.assertEquals(Integer.MAX_VALUE, capturedBlockingQueue.remainingCapacity());
     }
@@ -28,14 +52,14 @@ public class CapturedBlockingQueueTest {
     public void testBlockingQueueCapacitySet() {
         final int capacity = 100;
         BlockingQueue<Runnable> queue = new VariableLinkedBlockingQueue<>(capacity);
-        CapturedBlockingQueue capturedBlockingQueue = new CapturedBlockingQueue(queue);
+        CapturedBlockingQueue capturedBlockingQueue = new CapturedBlockingQueue(dtpExecutor);
         Assertions.assertEquals(capacity, capturedBlockingQueue.remainingCapacity());
     }
 
     @Test
     public void testPut() {
         BlockingQueue<Runnable> queue = new VariableLinkedBlockingQueue<>();
-        CapturedBlockingQueue capturedBlockingQueue = new CapturedBlockingQueue(queue);
+        CapturedBlockingQueue capturedBlockingQueue = new CapturedBlockingQueue(dtpExecutor);
         Assertions.assertThrows(UnsupportedOperationException.class, () ->
                 capturedBlockingQueue.put(() -> System.out.println("can't put Runnable to CapturedBlockingQueue")));
     }
@@ -43,7 +67,7 @@ public class CapturedBlockingQueueTest {
     @Test
     public void testTake() {
         BlockingQueue<Runnable> queue = new VariableLinkedBlockingQueue<>();
-        CapturedBlockingQueue capturedBlockingQueue = new CapturedBlockingQueue(queue);
+        CapturedBlockingQueue capturedBlockingQueue = new CapturedBlockingQueue(dtpExecutor);
         Assertions.assertThrows(UnsupportedOperationException.class, capturedBlockingQueue::take);
     }
 
@@ -55,7 +79,7 @@ public class CapturedBlockingQueueTest {
 
         BlockingQueue<Runnable> queue = new VariableLinkedBlockingQueue<>(capacity);
         putTaskToQueue(firstPutSize, queue);
-        CapturedBlockingQueue capturedBlockingQueue = new CapturedBlockingQueue(queue);
+        CapturedBlockingQueue capturedBlockingQueue = new CapturedBlockingQueue(dtpExecutor);
 
         Assertions.assertEquals(capacity - firstPutSize, capturedBlockingQueue.remainingCapacity());
         Assertions.assertEquals(firstPutSize, capturedBlockingQueue.size());

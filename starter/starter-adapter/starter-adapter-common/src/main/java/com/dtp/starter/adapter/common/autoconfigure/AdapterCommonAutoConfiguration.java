989ef5dad96cbb9ff2fa74245a2da5bca0907e07
yanhom <yanhom1314@foxmail.com>
2023-04-23 13:03:36 +0800

    fix

--- starter/starter-adapter/starter-adapter-common/src/main/java/com/dtp/starter/adapter/common/autoconfigure/AdapterCommonAutoConfiguration.java
+++ starter/starter-adapter/starter-adapter-common/src/main/java/com/dtp/starter/adapter/common/autoconfigure/AdapterCommonAutoConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 
 /**
  * AdapterCommonAutoConfiguration related
@@ -24,7 +23,6 @@ import org.springframework.context.annotation.DependsOn;
 public class AdapterCommonAutoConfiguration {
 
     @Bean
-    @DependsOn({"dtpApplicationContextHolder"})
     @ConditionalOnMissingBean
     public DtpAdapterListener dtpAdapterListener() {
         return new DtpAdapterListener();

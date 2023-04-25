c77a6915a1cc9fd406cbf96bbaa9d930a238a26a
yanhom <yanhom1314@foxmail.com>
2023-04-25 22:43:50 +0800

    update test case

--- test/test-core/src/test/java/org/dromara/dynamictp/test/core/thread/EagerDtpExecutorTest.java
+++ test/test-core/src/test/java/org/dromara/dynamictp/test/core/thread/EagerDtpExecutorTest.java
@@ -1,36 +1,22 @@
-package com.dtp.test.core.thread;
+package org.dromara.dynamictp.test.core.thread;
 
-import com.dtp.common.em.NotifyItemEnum;
-import com.dtp.core.DtpRegistry;
-import com.dtp.core.notifier.manager.AlarmManager;
-import com.dtp.core.spring.EnableDynamicTp;
-import com.dtp.core.spring.YamlPropertySourceFactory;
-import com.dtp.core.support.ExecutorWrapper;
-import com.dtp.core.thread.DtpExecutor;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Tag;
+import org.dromara.dynamictp.core.DtpRegistry;
+import org.dromara.dynamictp.core.spring.EnableDynamicTp;
+import org.dromara.dynamictp.core.spring.YamlPropertySourceFactory;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.MockedStatic;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.mockStatic;
-
 /**
- * DtpExecutorTest related
+ * EagerDtpExecutorTest related
  *
- * @author yanhom
  * @author kamtohung
  * @since 1.1.0
  */
@@ -40,7 +26,7 @@ import static org.mockito.Mockito.mockStatic;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = EagerDtpExecutorTest.class)
 @PropertySource(value = "classpath:/dynamic-tp-demo.yml", factory = YamlPropertySourceFactory.class)
-public class EagerDtpExecutorTest {
+class EagerDtpExecutorTest {
 
     @Test
     void test() throws InterruptedException {
@@ -54,8 +40,5 @@ public class EagerDtpExecutorTest {
                 }
             });
         }
-//        new CountDownLatch(1).await();
     }
-
-
 }

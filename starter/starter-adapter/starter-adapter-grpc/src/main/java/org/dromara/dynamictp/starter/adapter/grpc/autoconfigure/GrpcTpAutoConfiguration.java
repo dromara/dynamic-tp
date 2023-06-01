03d667b8e286c365c8fbd516f276b3ecf1027401
hailang <a1936978077@hotmail.com>
2023-06-01 12:41:17 +0800

    grpc support springmvc

--- starter/starter-adapter/starter-adapter-grpc/src/main/java/org/dromara/dynamictp/starter/adapter/grpc/autoconfigure/GrpcTpAutoConfiguration.java
+++ starter/starter-adapter/starter-adapter-grpc/src/main/java/org/dromara/dynamictp/starter/adapter/grpc/autoconfigure/GrpcTpAutoConfiguration.java
@@ -22,7 +22,6 @@ import org.dromara.dynamictp.core.spring.DtpBaseBeanConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -33,7 +32,6 @@ import org.springframework.context.annotation.Configuration;
  * @since 1.0.9
  */
 @Configuration
-@ConditionalOnProperty(prefix = "grpc.server", value = {"port"})
 @ConditionalOnBean({DtpBaseBeanConfiguration.class})
 @AutoConfigureAfter({DtpBaseBeanConfiguration.class})
 @SuppressWarnings("all")

86f988235eed54391286d6d46ec1af0866d6c5a2
wuhui <wuhui@88.com>
2023-11-27 14:00:40 +0800

    1、去掉DtpProperties中nacos对象的配置 2、引入 <dependency>     <groupId>com.alibaba.boot</groupId>      <artifactId>nacos-config-spring-boot-actuator</artifactId> </dependency> 依赖 重写 NacosRefresher 实现SmartApplicationListener接口，监听NacosConfigEvent事件，当nacos服务器中配置发生变更时回接受到此事件，调用AbstractRefresher类中的refresh即可

--- starter/starter-configcenter/starter-nacos/src/main/java/org/dromara/dynamictp/starter/nacos/autoconfigure/DtpNacosAutoConfiguration.java
+++ starter/starter-configcenter/starter-nacos/src/main/java/org/dromara/dynamictp/starter/nacos/autoconfigure/DtpNacosAutoConfiguration.java
@@ -21,9 +21,7 @@ import org.dromara.dynamictp.core.spring.DtpBaseBeanConfiguration;
 import org.dromara.dynamictp.starter.nacos.refresher.NacosRefresher;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -34,8 +32,6 @@ import org.springframework.context.annotation.Configuration;
  * @since 1.0.0
  **/
 @Configuration
-@ConditionalOnClass(value = com.alibaba.nacos.api.config.ConfigService.class)
-@ConditionalOnMissingClass(value = {"com.alibaba.cloud.nacos.NacosConfigProperties"})
 @ConditionalOnBean({DtpBaseBeanConfiguration.class})
 @AutoConfigureAfter({DtpBaseBeanConfiguration.class})
 public class DtpNacosAutoConfiguration {

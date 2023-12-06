ac3f989bf6df8b4b0207728281c15d6dd7f2ea82
BanTanger <1290288968@qq.com>
2023-12-06 10:11:41 +0800

    refactor ApolloRefresher

--- starter/starter-configcenter/starter-apollo/src/main/java/org/dromara/dynamictp/starter/apollo/refresher/ApolloRefresher.java
+++ starter/starter-configcenter/starter-apollo/src/main/java/org/dromara/dynamictp/starter/apollo/refresher/ApolloRefresher.java
@@ -17,75 +17,44 @@
 
 package org.dromara.dynamictp.starter.apollo.refresher;
 
-import com.ctrip.framework.apollo.ConfigFile;
-import com.ctrip.framework.apollo.ConfigFileChangeListener;
-import com.ctrip.framework.apollo.ConfigService;
-import com.ctrip.framework.apollo.core.enums.ConfigFileFormat;
-import com.ctrip.framework.apollo.model.ConfigFileChangeEvent;
-import org.dromara.dynamictp.common.properties.DtpProperties;
-import org.dromara.dynamictp.common.em.ConfigFileTypeEnum;
-import org.dromara.dynamictp.core.refresher.AbstractRefresher;
+import com.ctrip.framework.apollo.ConfigChangeListener;
+import com.ctrip.framework.apollo.model.ConfigChangeEvent;
+import com.ctrip.framework.apollo.spring.config.ConfigPropertySource;
+import com.ctrip.framework.apollo.spring.config.ConfigPropertySourceFactory;
+import com.ctrip.framework.apollo.spring.util.SpringInjector;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
+import org.dromara.dynamictp.core.refresher.AbstractRefresher;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * ApolloRefresher related
+ * <p>Listen for configuration file changes</p>
  *
  * @author yanhom
  * @since 1.0.0
  **/
 @Slf4j
-public class ApolloRefresher extends AbstractRefresher implements ConfigFileChangeListener, InitializingBean {
+public class ApolloRefresher extends AbstractRefresher implements ConfigChangeListener, InitializingBean {
 
-    @Value("${apollo.bootstrap.namespaces:application}")
-    private String namespace;
-
-    private ConfigFileTypeEnum configFileType;
+    private final ConfigPropertySourceFactory configPropertySourceFactory = SpringInjector
+            .getInstance(ConfigPropertySourceFactory.class);
 
     @Override
-    public void afterPropertiesSet() {
-
-        String[] apolloNamespaces = this.namespace.split(",");
-        String realNamespace = apolloNamespaces[0];
-        DtpProperties.Apollo apollo = dtpProperties.getApollo();
-        if (apollo != null && StringUtils.isNotBlank(apollo.getNamespace())) {
-            realNamespace = apollo.getNamespace();
-        }
-
-        configFileType = deduceFileType(realNamespace);
-        namespace = realNamespace.replaceAll("." + configFileType.getValue(), "");
-        ConfigFileFormat configFileFormat = ConfigFileFormat.fromString(configFileType.getValue());
-        ConfigFile configFile = ConfigService.getConfigFile(namespace, configFileFormat);
-        try {
-            configFile.addChangeListener(this);
-            log.info("DynamicTp refresher, add listener success, namespace: {}", realNamespace);
-        } catch (Exception e) {
-            log.error("DynamicTp refresher, add listener error, namespace: {}", realNamespace, e);
-        }
+    public void onChange(ConfigChangeEvent changeEvent) {
+        refresh(environment);
     }
 
     @Override
-    public void onChange(ConfigFileChangeEvent changeEvent) {
-        String content = changeEvent.getNewValue();
-        refresh(content, configFileType);
-    }
-
-    private ConfigFileTypeEnum deduceFileType(String namespace) {
-        ConfigFileTypeEnum configFileFormat = ConfigFileTypeEnum.PROPERTIES;
-        if (namespace.contains(ConfigFileTypeEnum.YAML.getValue())) {
-            configFileFormat = ConfigFileTypeEnum.YAML;
-        } else if (namespace.contains(ConfigFileTypeEnum.YML.getValue())) {
-            configFileFormat = ConfigFileTypeEnum.YML;
-        } else if (namespace.contains(ConfigFileTypeEnum.JSON.getValue())) {
-            configFileFormat = ConfigFileTypeEnum.JSON;
-        } else if (namespace.contains(ConfigFileTypeEnum.XML.getValue())) {
-            configFileFormat = ConfigFileTypeEnum.XML;
-        } else if (namespace.contains(ConfigFileTypeEnum.TXT.getValue())) {
-            configFileFormat = ConfigFileTypeEnum.TXT;
+    public void afterPropertiesSet() throws Exception {
+        List<ConfigPropertySource> configPropertySources = configPropertySourceFactory.getAllConfigPropertySources();
+        for (ConfigPropertySource configPropertySource : configPropertySources) {
+            if (Arrays.stream(configPropertySource.getPropertyNames())
+                    .anyMatch(propertyName -> propertyName.startsWith("spring.dynamic.tp"))) {
+                configPropertySource.addChangeListener(this);
+            }
         }
-
-        return configFileFormat;
     }
 }

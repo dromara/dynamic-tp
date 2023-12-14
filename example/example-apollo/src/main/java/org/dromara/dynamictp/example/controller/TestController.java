d87df10abb079f7fc39fd284c835ff5aad7d3c1b
yanhom <yanhom1314@foxmail.com>
2023-12-14 08:30:38 +0800

    perfect example

--- example/example-apollo/src/main/java/org/dromara/dynamictp/example/controller/TestController.java
+++ example/example-apollo/src/main/java/org/dromara/dynamictp/example/controller/TestController.java
@@ -17,48 +17,55 @@
 
 package org.dromara.dynamictp.example.controller;
 
-import org.dromara.dynamictp.core.DtpRegistry;
-import org.dromara.dynamictp.core.support.task.runnable.NamedRunnable;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.dromara.dynamictp.example.service.TestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @author Redick01
  */
 @Slf4j
 @RestController
-@SuppressWarnings("all")
+@AllArgsConstructor
 public class TestController {
 
-    @Resource
-    private ThreadPoolExecutor dtpExecutor1;
+    private final TestService testService;
 
-    @GetMapping("/dtp-nacos-example/test")
-    public String test() throws InterruptedException {
-        task();
-        return "success";
+    @GetMapping("/dtp-apollo-example/testJucTp")
+    public String testJuc() {
+        testService.testJucTp();
+        return "testJucTp success";
     }
 
-    public void task() throws InterruptedException {
-        Executor dtpExecutor2 = DtpRegistry.getExecutor("dtpExecutor2");
-        for (int i = 0; i < 100; i++) {
-            Thread.sleep(100);
-            dtpExecutor1.execute(() -> {
-                log.info("i am dynamic-tp-test-1 task");
-            });
-            dtpExecutor2.execute(NamedRunnable.of(() -> {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                log.info("i am dynamic-tp-test-2 task");
-            }, "task-" + i));
-        }
+    @GetMapping("/dtp-apollo-example/testSpringTp")
+    public String testSpring() {
+        testService.testSpringTp();
+        return "testSpringTp success";
+    }
+
+    @GetMapping("/dtp-apollo-example/testCommonDtp")
+    public String testCommon() {
+        testService.testCommonDtp();
+        return "testCommonDtp success";
+    }
+
+    @GetMapping("/dtp-apollo-example/testEagerDtp")
+    public String testEager() {
+        testService.testEagerDtp();
+        return "testEagerDtp success";
+    }
+
+    @GetMapping("/dtp-apollo-example/testScheduledDtp")
+    public String testScheduled() {
+        testService.testScheduledDtp();
+        return "testScheduledDtp success";
+    }
+
+    @GetMapping("/dtp-apollo-example/testOrderedDtp")
+    public String testOrdered() {
+        testService.testOrderedDtp();
+        return "testOrderedDtp success";
     }
 }

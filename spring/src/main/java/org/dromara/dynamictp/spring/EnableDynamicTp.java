222953f7fa1781c231828e0121fa5cc2ff139e28
itcast <17338548613@163.com>
2024-07-25 20:18:10 +0800

    refactor：import*优化

--- spring/src/main/java/org/dromara/dynamictp/spring/EnableDynamicTp.java
+++ spring/src/main/java/org/dromara/dynamictp/spring/EnableDynamicTp.java
@@ -19,7 +19,11 @@ package org.dromara.dynamictp.spring;
 
 import org.springframework.context.annotation.Import;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * EnableDynamicTp related

2f7a99deaf1929bbfecefa0bc75fa4b1fb7dee1e
TheFatRatre <1981590178@qq.com>
2024-11-18 13:18:35 +0800

    format

--- common/src/main/java/org/dromara/dynamictp/common/entity/ExecutorStats.java
+++ common/src/main/java/org/dromara/dynamictp/common/entity/ExecutorStats.java
@@ -54,11 +54,6 @@ public class ExecutorStats extends Metrics {
     private String poolAliasName;
 
     /**
-     * 是否为虚拟线程执行器
-     */
-    private boolean isVirtualExecutor;
-
-    /**
      * 核心线程数
      */
     private int corePoolSize;
@@ -67,6 +62,31 @@ public class ExecutorStats extends Metrics {
      * 最大线程数
      */
     private int maximumPoolSize;
+
+    /**
+     * 正在执行任务的活跃线程大致总数
+     */
+    private int activeCount;
+
+    /**
+     * 大致任务总数
+     */
+    private long taskCount;
+
+    /**
+     * 执行超时任务数量
+     */
+    private long runTimeoutCount;
+
+    /**
+     * 是否为DtpExecutor
+     */
+    private boolean dynamic;
+
+    /**
+     * 是否为虚拟线程执行器
+     */
+    private boolean isVirtualExecutor;
 
     /**
      * 空闲时间 (ms)
@@ -132,5 +152,55 @@ public class ExecutorStats extends Metrics {
      * 在队列等待超时任务数量
      */
     private long queueTimeoutCount;
+
+    /**
+     * tps
+     */
+    private double tps;
+
+    /**
+     * 最大任务耗时
+     */
+    private long maxRt;
+
+    /**
+     * 最小任务耗时
+     */
+    private long minRt;
+
+    /**
+     * 任务平均耗时(单位:ms)
+     */
+    private double avg;
+
+    /**
+     * 满足50%的任务执行所需的最低耗时
+     */
+    private double tp50;
+
+    /**
+     * 满足75%的任务执行所需的最低耗时
+     */
+    private double tp75;
+
+    /**
+     * 满足90%的任务执行所需的最低耗时
+     */
+    private double tp90;
+
+    /**
+     * 满足95%的任务执行所需的最低耗时
+     */
+    private double tp95;
+
+    /**
+     * 满足99%的任务执行所需的最低耗时
+     */
+    private double tp99;
+
+    /**
+     * 满足99.9%的任务执行所需的最低耗时
+     */
+    private double tp999;
 
 }

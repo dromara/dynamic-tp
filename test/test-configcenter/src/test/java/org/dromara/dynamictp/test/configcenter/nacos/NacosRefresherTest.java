8434aa5963a330e9767ac9a0ea4e9ebac7e77150
yanhom <yanhom1314@foxmail.com>
2026-06-17 11:34:29 +0800

    Fix nacos refresher test on Spring Boot 3

--- test/test-configcenter/src/test/java/org/dromara/dynamictp/test/configcenter/nacos/NacosRefresherTest.java
+++ test/test-configcenter/src/test/java/org/dromara/dynamictp/test/configcenter/nacos/NacosRefresherTest.java
@@ -17,16 +17,17 @@
 
 package org.dromara.dynamictp.test.configcenter.nacos;
 
-import com.alibaba.nacos.api.config.ConfigType;
-import com.alibaba.nacos.api.config.listener.AbstractListener;
-import com.alibaba.nacos.api.config.listener.Listener;
 import com.alibaba.nacos.spring.context.event.config.NacosConfigReceivedEvent;
-import com.alibaba.nacos.spring.core.env.NacosPropertySource;
 import org.dromara.dynamictp.test.configcenter.DtpBaseTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.io.ByteArrayResource;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 import java.util.concurrent.ThreadPoolExecutor;
 
 import static org.mockito.Mockito.mock;
@@ -48,32 +49,30 @@ class NacosRefresherTest extends DtpBaseTest {
         Thread.sleep(2000L);
         corePoolSize = context.getBean("dtpExecutor1", ThreadPoolExecutor.class).getCorePoolSize();
         System.out.println("nacos refresher, corePoolSize after refresh: " + corePoolSize);
+        Assertions.assertEquals(10, corePoolSize);
     }
 
     private void mockConfigChange() {
         String dataId = "dynamic-tp-demo-dtp-dev.yml";
-        String groupId = "DEFAULT_GROUP";
-        String type = ConfigType.YAML.getType();
         String content =
                 "dynamictp:\n" +
-                "      enabled: true                               # 是否启用 dynamictp，默认true\n" +
-                "      executors:                                  # 动态线程池配置，都有默认值，采用默认值的可以不配置该项，减少配置量\n" +
+                "      enabled: true\n" +
+                "      executors:\n" +
                 "        - threadPoolName: dtpExecutor1\n" +
-                "          threadPoolAliasName: 测试线程池        # 线程池别名\n" +
-                "          executorType: common                 # 线程池类型 common、eager、ordered、scheduled，默认 common\n" +
-                "          corePoolSize: 10                      # 核心线程数，默认1\n" +
-                "          maximumPoolSize: 20                   # 最大线程数，默认cpu核数\n";
+                "          threadPoolAliasName: test\n" +
+                "          executorType: common\n" +
+                "          corePoolSize: 10\n" +
+                "          maximumPoolSize: 20\n";
 
-        Listener listener = new AbstractListener() {
-            @Override
-            public void receiveConfigInfo(String config) {
-                NacosPropertySource newNacosPropertySource = new NacosPropertySource(dataId, groupId, dataId, config, type);
-                MutablePropertySources propertySources = environment.getPropertySources();
-                // replace NacosPropertySource
-                propertySources.replace(dataId, newNacosPropertySource);
-            }
-        };
-        listener.receiveConfigInfo(content);
+        MutablePropertySources propertySources = environment.getPropertySources();
+        propertySources.replace(dataId, createYamlPropertySource(dataId, content));
         publisher.publishEvent(mock(NacosConfigReceivedEvent.class));
+    }
+
+    private PropertiesPropertySource createYamlPropertySource(String dataId, String content) {
+        YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+        factory.setResources(new ByteArrayResource(content.getBytes(StandardCharsets.UTF_8)));
+        Properties properties = factory.getObject();
+        return new PropertiesPropertySource(dataId, properties);
     }
 }
